0291bbd55314fc0b647013346736077543e4d5b4
Daniel Garza <danielrgarza12@gmail.com>
2023-10-11 18:36:09 -0500

    Update index.php

--- index.php
+++ index.php
@@ -2,17 +2,22 @@
 <html>
 <head>
     <title>Login</title>
+    <link rel="stylesheet" href="styles.css"> <!-- Assuming the CSS file is in the same directory as the PHP file -->
 </head>
 <body>
 
 <h2>Login Page</h2>
 
 <form action="validate_login.php" method="post">
-  Username:<br>
-  <input type="text" name="username">
+  <div>
+    <label for="username">Username:</label><br>
+    <input type="text" id="username" name="username">
+  </div>
   <br>
-  Password:<br>
-  <input type="password" name="password">
+  <div>
+    <label for="password">Password:</label><br>
+    <input type="password" id="password" name="password">
+  </div>
   <br><br>
   <input type="submit" value="Login">
 </form> 
