c58b851fa4193b887b43a14f971dc1c928c5bc15
Daniel Garza <danielrgarza12@gmail.com>
2023-10-11 00:26:00 -0500

    created style.css

--- index.php
+++ index.php
@@ -4,16 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hello World</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-            background-color: #f4f4f4;
-        }
-    </style>
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
     <h1>IT WORKS!!!!</h1>
