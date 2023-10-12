07b1115b69cb57ac2ef6b6ed19e764a9a7097be8
Daniel Garza <danielrgarza12@gmail.com>
2023-10-11 19:25:50 -0500

    changed login

--- index.php
+++ index.php
@@ -2,34 +2,36 @@
 <html>
 <head>
     <title>Login</title>
-    <link rel="stylesheet" href="styles.css"> <!-- Assuming the CSS file is in the same directory as the PHP file -->
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
 
-  
-  <form action="validate_login.php" method="post">
-  <h2 align="center">Login Page</h2>
-  <div>
-    <label for="username">Username:</label><br>
-    <input 
-      type="text" 
-      id="username" 
-      name="username"
-      placeholder="Enter username"
-      required>
-  </div>
-  <br>
-  <div>
-    <label for="password">Password:</label><br>
-    <input 
-      type="password" 
-      id="password"
-      name="password"
-      placeholder="Enter password"
-      required>
-  </div>
-  <br><br>
-  <input type="submit" value="Login">
+<form action="validate_login.php" method="post">
+    <h2>Login Page</h2>
+    <div>
+        <label for="username">Username:</label><br>
+        <input 
+            type="text" 
+            id="username" 
+            name="username"
+            placeholder="Enter username"
+            required>
+    </div>
+    <br>
+    <div>
+        <label for="password">Password:</label><br>
+        <input 
+            type="password" 
+            id="password"
+            name="password"
+            placeholder="Enter password"
+            required>
+    </div>
+    <br><br>
+    <input type="submit" value="Login">
+
+    <br>
+    <p>First time customer? <a href="/sign_up">Sign up now and start earning!</a></p>
 </form> 
 
 </body>
