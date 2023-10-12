8ec1877390510b99c21ccec4f15c6c605a949a48
Daniel Garza <danielrgarza12@gmail.com>
2023-10-12 03:31:19 -0500

    Update signup.php

--- signup.php
+++ signup.php
@@ -11,57 +11,46 @@
 <form method="post">
     <h2>Create your POS Pizza Account</h2>
     <div>
-        <label for="fullname">Full Name</label><br>
-        <input 
-            type="text" 
-            id="fullname" 
-            name="fullname"
-            placeholder="Enter full name"
-            required>
+        <label for="firstname">First Name</label><br>
+        <input type="text" id="firstname" name="firstname" placeholder="Enter first name" required>
+    </div>
+    <br>
+
+    <div>
+        <label for="lastname">Last Name</label><br>
+        <input type="text" id="lastname" name="lastname" placeholder="Enter last name" required>
+    </div>
+    <br>
+
+    <div>
+        <label for="mi">M.I.</label><br>
+        <input type="text" id="mi" name="mi" placeholder="Enter middle initial" maxlength="1">
     </div>
     <br>
     <div>
         <label for="email">Email</label><br>
-        <input 
-            type="email" 
-            id="email" 
-            name="email"
-            placeholder="Enter email address"
-            required>
+        <input type="email" id="email" name="email" placeholder="Enter email address" required>
     </div>
     <br>
     <div>
         <label for="username">Username</label><br>
-        <input 
-            type="text" 
-            id="username" 
-            name="username"
-            placeholder="Choose a username"
-            required>
+        <input type="text" id="username" name="username" placeholder="Choose a username" required>
     </div>
     <br>
+
     <div>
         <label for="password">Password</label><br>
-        <input 
-            type="password" 
-            id="password"
-            name="password"
-            placeholder="Choose a password"
-            required>
+        <input type="password" id="password" name="password" placeholder="Choose a password" required>
     </div>
     <br>
+
     <div>
         <label for="confirmpassword">Confirm Password</label><br>
-        <input 
-            type="password" 
-            id="confirmpassword"
-            name="confirmpassword"
-            placeholder="Confirm your password"
-            required>
+        <input type="password" id="confirmpassword" name="confirmpassword" placeholder="Confirm your password" required>
     </div>
     <br>
-    <input type="submit" value="Sign Up">
 
+    <input type="submit" value="Sign Up">
 </form> 
 
 </body>
