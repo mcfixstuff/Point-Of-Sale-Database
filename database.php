aac35304af75b48968f2fd5b86d2d234707fccc8
Daniel Garza <danielrgarza12@gmail.com>
2023-10-12 20:28:37 -0500

    edited signup.php

--- database.php
+++ database.php
@@ -10,19 +10,3 @@ if (!$mysqli->real_connect($host, $username, $password, $dbname, $port, NULL, MY
     die("Connection error: " . $mysqli->connect_error);
 }
 return $mysqli;
-
-/*
-// Sample query to test connection and fetch data
-$query = "SELECT * FROM your_table_name"; // replace 'your_table_name' with one of your table names
-$result = mysqli_query($con, $query);
-
-if ($result) {
-    while ($row = mysqli_fetch_assoc($result)) {
-        // Example: Display each row's data
-        echo "Data: " . $row['your_column_name'] . "<br>"; // replace 'your_column_name' with one of your column names
-    }
-    mysqli_free_result($result);
-} else {
-    echo "Error: " . mysqli_error($con);
-}
-// */
--- signup.php
+++ signup.php
@@ -1,15 +1,8 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-
 $host = 'pospizza.mysql.database.azure.com';
 $dbname = 'pos';
 $username = 'danielgarza';
 $password = '#drgarza8';
-
-$message = '';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     try {
