14081e351ee9e886176f42f8322d8a801d1ac607
Daniel Garza <108763564+danielraygarza@users.noreply.github.com>
2023-10-13 09:34:51 -0500

    Delete inventory.sql

--- inventory.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS INVENTORY (
-    Inventory_ID INT AUTO_INCREMENT PRIMARY KEY,
-    Item_Name VARCHAR(255) NOT NULL,
-    Amount INT NOT NULL,
-    Reorder_Threshold INT NOT NULL,
-    Last_Stock_Shipment_Date DATE,
-    Expiration_Date DATE
-);
