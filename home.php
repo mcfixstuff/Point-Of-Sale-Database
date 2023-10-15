22f4202c0e539ca4b5a9aba92594e74d9376e000
Daniel Garza <danielrgarza12@gmail.com>
2023-10-15 00:52:55 -0500

    created home.php

---
+++ home.php
@@ -0,0 +1,20 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>POS Pizza</title>
+    <link rel="stylesheet" href="styles.css">
+    <link rel="icon" href="img/pizza.ico" type="image/x-icon">
+</head>
+<body>
+
+<form action="" method="post">
+    <h2>Thank you for joining POS Pizza family!</h2>
+
+    <a class="button">Order now!</a>
+    
+    </div>
+</form> 
+
+</body>
+</html>
--- signup.php
+++ signup.php
@@ -48,7 +48,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") { // Check if the form has been submit
         <!-- <a href="#">Order Now</a>
         <a href="#">Profile</a> -->
     </div>
-    <form action="signup.php" method="post">
+    <form action="home.php" method="post">
         <h2>Create your POS Pizza Account</h2>
         <div>       
             <label for="first_name">Name  </label>
