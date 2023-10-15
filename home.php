27c080b9ef9304b49cd6caa9a6accd63e8fe6df9
Daniel Garza <danielrgarza12@gmail.com>
2023-10-15 00:57:09 -0500

    create valid_signup.php

--- home.php
+++ home.php
@@ -7,7 +7,12 @@
     <link rel="icon" href="img/pizza.ico" type="image/x-icon">
 </head>
 <body>
-
+    <div class="navbar">
+        <a href="index.php">Home</a>
+        <!-- <a href="#">Order Now</a>
+        <a href="#">Profile</a> -->
+    </div>
+    
 <form action="" method="post">
     <h2>Thank you for joining POS Pizza family!</h2>
 
--- signup.php
+++ signup.php
@@ -1,34 +1,4 @@
-<?php
-include 'database.php'; // Include the database connection details
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") { // Check if the form has been submitted
-
-    // Extracting data from the form
-    $first_name = $mysqli->real_escape_string($_POST['first_name']);
-    $middle_initial = $mysqli->real_escape_string($_POST['middle_initial']);
-    $last_name = $mysqli->real_escape_string($_POST['last_name']);
-    $birthday_month = $mysqli->real_escape_string($_POST['birthday_month']);
-    $birthday_day = $mysqli->real_escape_string($_POST['birthday_day']);
-    $address = $mysqli->real_escape_string($_POST['address']);
-    $address2 = $mysqli->real_escape_string($_POST['address2']);
-    $city = $mysqli->real_escape_string($_POST['city']);
-    $state = $mysqli->real_escape_string($_POST['state']);
-    $zip_code = $mysqli->real_escape_string($_POST['zip_code']);
-    $phone_number = $mysqli->real_escape_string($_POST['phone_number']);
-    $email = $mysqli->real_escape_string($_POST['email']);
-    $password = password_hash($mysqli->real_escape_string($_POST['password']), PASSWORD_DEFAULT); // Hashing the password before storing it in the database
-
-    // Inserting the data into the database
-    $sql = "INSERT INTO users_table_name (first_name, middle_initial, last_name, birthday_month, birthday_day, address, address2, city, state, zip_code, phone_number, email, password) 
-            VALUES ('$first_name', '$middle_initial', '$last_name', '$birthday_month', '$birthday_day', '$address', '$address2', '$city', '$state', '$zip_code', '$phone_number', '$email', '$password')";
-
-    if ($mysqli->query($sql) === TRUE) {
-        echo "Account created successfully!";
-    } else {
-        echo "Error: " . $sql . "<br>" . $mysqli->error;
-    }
-}
-?>
 
 
 <!-- Signup page for new users -->
