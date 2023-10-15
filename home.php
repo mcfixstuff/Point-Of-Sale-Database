8c197f5242200f26f875a48f63f2a975b857a6de
Daniel Garza <danielrgarza12@gmail.com>
2023-10-15 03:24:30 -0500

    Update home.php

--- home.php
+++ home.php
@@ -1,6 +1,7 @@
-<?php
-session_start();
+<?php 
+session_start(); 
 ?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -11,19 +12,20 @@ session_start();
 <body>
     <div class="navbar">
         <a href="index.php">Home</a>
+        <!-- <a href="#">Order Now</a>
+        <a href="#">Profile</a> -->
     </div>
 
-    <?php
-    if(isset($_SESSION['first_name']) && !empty($_SESSION['first_name'])){
-        echo "<h2>Welcome back, " . htmlspecialchars($_SESSION['first_name']) . "!</h2>";
+    <?php 
+    // Check if the first_name is set in the session (or retrieve it from your database if needed)
+    if (isset($_SESSION['first_name'])) {
+        echo "<h2>Welcome back, " . $_SESSION['first_name'] . "!</h2>";
     } else {
         echo "<h2>Thank you for joining POS Pizza family!</h2>";
     }
     ?>
 
-    <form action="" method="post">
-        <a class="button">Order now!</a>
-    </form> 
+    <a class="button">Order now!</a>
 
 </body>
 </html>
