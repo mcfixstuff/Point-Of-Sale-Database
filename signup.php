99c9cb886c6806168b301dcd200e793a456d0187
Daniel Garza <danielrgarza12@gmail.com>
2023-10-15 00:59:48 -0500

    Update signup.php

--- signup.php
+++ signup.php
@@ -18,7 +18,7 @@
         <!-- <a href="#">Order Now</a>
         <a href="#">Profile</a> -->
     </div>
-    <form action="home.php" method="post">
+    <form action="valid_signup.php" method="post">
         <h2>Create your POS Pizza Account</h2>
         <div>       
             <label for="first_name">Name  </label>
@@ -127,8 +127,6 @@
         <div>
             <input class = button type="submit" value="Sign Up">
         </form>
-        </div><br>
-
 </body>
 </html>
 
