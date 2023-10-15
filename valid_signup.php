7745a43ad5d9269049fbf3f394863095b16bced5
Daniel Garza <danielrgarza12@gmail.com>
2023-10-15 01:03:19 -0500

    Update valid_signup.php

--- valid_signup.php
+++ valid_signup.php
@@ -29,3 +29,28 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") { // Check if the form has been submit
     }
 }
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>POS Pizza</title>
+    <link rel="stylesheet" href="styles.css">
+    <link rel="icon" href="img/pizza.ico" type="image/x-icon">
+</head>
+<body>
+    <div class="navbar">
+        <a href="index.php">Home</a>
+        <!-- <a href="#">Order Now</a>
+        <a href="#">Profile</a> -->
+    </div>
+    
+<form action="" method="post">
+    <h2>Thank you for joining POS Pizza family!</h2>
+
+    <a class="button">Order now!</a>
+    
+    </div>
+</form> 
+
+</body>
+</html>
