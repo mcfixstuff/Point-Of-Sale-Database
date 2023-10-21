8003bd79ad741a26cc2b17f4105cfe0525150008
Daniel Garza <danielrgarza12@gmail.com>
2023-10-21 00:47:03 -0500

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,7 +1,7 @@
 <?php
-// Effectively logout function for navbar and will send to home page and clear session
-session_start();
-session_destroy(); // This will clear the session
-header("Location: index.php"); // Redirecting to index.php
-exit();
+    //Logout file for nav bar. Will kill session and send to main page
+    session_start();
+    session_destroy(); // This will clear the session
+    header("Location: index.php"); // Redirecting to index.php
+    exit();
 ?>
