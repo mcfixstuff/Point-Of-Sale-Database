0d25be5151a3fda07c12dd520540270d3312ce4d
Daniel Garza <danielrgarza12@gmail.com>
2023-10-20 23:27:36 -0500

    Update menu.php

--- menu.php
+++ menu.php
@@ -1,4 +1,4 @@
-!DOCTYPE html>
+<!DOCTYPE html>
 <html>
 <head>
     <title>POS Pizza</title>
@@ -13,13 +13,15 @@
     </div>
 
     <?php 
-    include 'database.php';
-    // Check if the first_name is set in the session (or retrieve it from your database if needed)
-    if (isset($_SESSION['first_name'])) {
-        echo "<h2>Time to order, " . $_SESSION['first_name'] . "!</h2>";
-    } else {
-        echo "<h2>Ready to order</h2>";
-    }
+        include 'database.php';
+        // Check if the first_name is set in the session (or retrieve it from your database if needed)
+        $name = $mysqli->query("SELECT * FROM customers WHERE first_name='$first_name'");
+
+        if (isset($_SESSION['first_name'])) {
+            echo "<h2>Time to order, " . $_SESSION['first_name'] . "!</h2>";
+        } else {
+            echo "<h2>Ready to order</h2>";
+        }
     ?>
 
 </body>
