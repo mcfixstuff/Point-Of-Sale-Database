2c470e6fee11be3d2060993e0277ea753fbaab7d
sS373Nn <119764691+sS373Nn@users.noreply.github.com>
2023-10-21 12:00:49 -0500

    Create reports.php

---
+++ reports.php
@@ -0,0 +1,30 @@
+<?php 
+    // // Check if the user is not logged in
+    // if (!isset($_SESSION['first_name'])) {
+    //     // Redirect them to the login page
+    //     header('Location: login.php');
+    //     exit();
+    // } 
+?>
+<!-- Welcome page after user creates new account -->
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>POS Pizza</title>
+        <link rel="stylesheet" href="styles.css">
+        <link rel="icon" href="img/pizza.ico" type="image/x-icon">
+    </head>
+    <body>
+        <div class="navbar">
+            <a href="index.php">Home</a>
+            <!-- <a href="#">Order Now</a>
+            <a href="#">Profile</a> -->
+        </div>
+        
+    <form action="" method="post">
+        <h2>Reports Page</h2>
+        <!-- <a href="menu.php" class="button">Order now!</a> -->
+    </form> 
+
+    </body>
+</html>
